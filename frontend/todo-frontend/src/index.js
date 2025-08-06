c97f9185051ac46d0e2be5a2a24af77966f778eb
amehmathias <amehmathiasejeh40@gmail.com>
2025-08-06 14:41:55 +0100

    Enable PWA and build for production

--- frontend/todo-frontend/src/index.js
+++ frontend/todo-frontend/src/index.js
@@ -2,9 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+// Register service worker to enable PWA features
+serviceWorkerRegistration.register();
