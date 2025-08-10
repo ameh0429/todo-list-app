827e28eddd3c17c6e8a5759a50f43d04d468958e
amehmathias <amehmathiasejeh40@gmail.com>
2025-08-10 21:15:20 +0100

    latest commit

--- frontend/todo-frontend/src/services/api.js
+++ frontend/todo-frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'https://todo-list-application.up.railway.app/api';
+const API_BASE_URL = process.env.REACT_APP_API_URL || 'https://todo-list-application.up.railway.app/api' || "http://localhost:3000";
 
 export const api = {
   login: async (email, password) => {
